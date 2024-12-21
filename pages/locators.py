d911488be3f34825be2f1c8de5a6180eeabb6967
Anton.Abramov <abra96@work.com>
2024-12-21 13:28:10 +0300

    add open login page in test's body

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+
+class LoginPageLocators():
+    BTN_ENTER = (By.NAME, "login_submit")
+    LOGIN_FORM = (By.ID, "login_form")
+    REG_FORM = (By.ID, "register_form")
